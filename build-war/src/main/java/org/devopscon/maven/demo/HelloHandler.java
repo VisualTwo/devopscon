403b938031d0c3fb20e0c25074c3924288d027eb
VisualTwo <github@itseite.de>
2018-12-03 13:38:49 +0100

    Update HelloHandler.java

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -1,10 +1,8 @@
 package org.devopscon.maven.demo;
 
 public class HelloHandler {
-
 	public String sayHello() {
-		String hello = "Hello DevOpsCon members, What do you think about this conference?";
-		return hello;
+		return "Hello DevOpsCon members, What do you think about this conference?";
 	}
 
 }
